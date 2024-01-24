14167419731c4381c464a0239e23dee3a4d0d63e
manikantaL16 <157564962+manikantaL16@users.noreply.github.com>
2024-01-24 11:24:43 +0530

    pratical 2 main.py

---
+++ pratical 2 main.py
@@ -0,0 +1,6 @@
+l=int(input("enter length : ")) 
+w=int(input("enter width : ")) 
+area=l*w 
+perimeter=2*(l+w) 
+print("area of rectangle is: ",area) 
+print("perimeter of rectangle is: ",perimeter)
